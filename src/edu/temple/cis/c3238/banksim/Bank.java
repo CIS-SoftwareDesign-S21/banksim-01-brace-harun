46d9e3e3b6406c7b389e53093687b7dfdae672ee
Edward Brace <eddiebrace@verizon.net>
2021-02-19 18:42:37 -0500

    refactored tester method into its own thread class

--- src/edu/temple/cis/c3238/banksim/Bank.java
+++ src/edu/temple/cis/c3238/banksim/Bank.java
@@ -48,22 +48,6 @@ public class Bank {
         }
     }
 
-    public void test() {
-        int totalBalance = 0;
-        for (Account account : accounts) {
-            System.out.printf("%-30s %s%n", 
-                    Thread.currentThread().toString(), account.toString());
-            totalBalance += account.getBalance();
-        }
-        System.out.printf("%-30s Total balance: %d\n", Thread.currentThread().toString(), totalBalance);
-        if (totalBalance != numAccounts * initialBalance) {
-            System.out.printf("%-30s Total balance changed!\n", Thread.currentThread().toString());
-            System.exit(0);
-        } else {
-            System.out.printf("%-30s Total balance unchanged.\n", Thread.currentThread().toString());
-        }
-    }
-
     public int getNumAccounts() {
         return numAccounts;
     }
@@ -71,6 +55,44 @@ public class Bank {
     
     public boolean shouldTest() {
         return ++numTransactions % NTEST == 0;
+    }
+
+    public void test() {
+        new Tester( accounts, initialBalance, numAccounts, Thread.currentThread() ).start();
+    }
+
+}
+
+
+class Tester extends Thread {
+
+    private final Account[] accounts;
+    private final int initialBalance;
+    private final int numAccounts;
+    private Thread transferThread;
+
+    public Tester( Account[] accounts, final int intialBalance, final int numAccounts, Thread transferThread ) {
+        this.accounts = accounts;
+        this.initialBalance = intialBalance;
+        this.numAccounts = numAccounts;
+        this.transferThread = transferThread;
+    }
+
+    @Override
+    public void run() {
+        int totalBalance = 0;
+        for (Account account : accounts) {
+            System.out.printf("%-30s %s%n",
+                    transferThread.toString(), account.toString());
+            totalBalance += account.getBalance();
+        }
+        System.out.printf("%-30s Total balance: %d\n", transferThread.toString(), totalBalance);
+        if (totalBalance != numAccounts * initialBalance) {
+            System.out.printf("%-30s Total balance changed!\n", transferThread.toString());
+            System.exit(0);
+        } else {
+            System.out.printf("%-30s Total balance unchanged.\n", transferThread.toString());
+        }
     }
 
 }
